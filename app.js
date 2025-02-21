4a4a751be1839a1b955eb35f37de37b2591d1f2e
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2025-02-20 22:54:19 -0500

    t

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta principal
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.php'));
 });
 
 // Iniciar el servidor
