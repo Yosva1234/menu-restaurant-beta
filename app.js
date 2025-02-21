a726787aee3de6662f41ea00ac0d2e4815c54d5e
Yosva1234 <158980095+Yosva1234@users.noreply.github.com>
2025-02-20 23:09:43 -0500

    5

--- app.js
+++ app.js
@@ -8,21 +8,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configurar la conexión a la base de datos MySQL
-const connection = mysql.createConnection({
-  host: 'bajha1aig69hxddhmuba-mysql.services.clever-cloud.com',// Dirección del servidor de la base de datos
-  user: 'ulzgopm4nysnylob',            // Usuario de la base de datos
-  password: 'MHrcAEuCvfUWJ0GvGNiH', // Contraseña del usuario
-  database: 'bajha1aig69hxddhmuba' // Nombre de la base de datos
-});
+// const connection = mysql.createConnection({
+//   host: 'bajha1aig69hxddhmuba-mysql.services.clever-cloud.com',// Dirección del servidor de la base de datos
+//   user: 'ulzgopm4nysnylob',            // Usuario de la base de datos
+//   password: 'MHrcAEuCvfUWJ0GvGNiH', // Contraseña del usuario
+//   database: 'bajha1aig69hxddhmuba' // Nombre de la base de datos
+// });
 
 // Conectar a la base de datos
-connection.connect((err) => {
-  if (err) {
-    console.error('Error al conectar a la base de datos:', err.stack);
-    return;
-  }
-  console.log('Conectado a la base de datos MySQL.');
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.error('Error al conectar a la base de datos:', err.stack);
+//     return;
+//   }
+//   console.log('Conectado a la base de datos MySQL.');
+// });
 
 // Servir archivos estáticos desde la carpeta "public"
 app.use(express.static(path.join(__dirname, 'public')));
@@ -33,18 +33,18 @@ app.get('/', (req, res) => {
 });
 
 // Ruta de ejemplo: obtener datos de la base de datos
-app.get('/bebidas', (req, res) => {
-  const query = 'SELECT * FROM bebidas'; // Cambia "usuarios" por el nombre de tu tabla
+// app.get('/bebidas', (req, res) => {
+//   const query = 'SELECT * FROM bebidas'; // Cambia "usuarios" por el nombre de tu tabla
 
-  connection.query(query, (err, results) => {
-    if (err) {
-      console.error('Error al ejecutar la consulta:', err.stack);
-      res.status(500).send('Error en el servidor');
-      return;
-    }
-    res.json(results); // Enviar los resultados como JSON
-  });
-});
+//   connection.query(query, (err, results) => {
+//     if (err) {
+//       console.error('Error al ejecutar la consulta:', err.stack);
+//       res.status(500).send('Error en el servidor');
+//       return;
+//     }
+//     res.json(results); // Enviar los resultados como JSON
+//   });
+// });
 
 // Iniciar el servidor
 app.listen(PORT, () => {
